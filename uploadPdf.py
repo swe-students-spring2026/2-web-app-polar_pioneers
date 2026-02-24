e6b6a1f665a541c9fc9af79470f17339e820d56f
rehanguptaNYU <rehang2005@gmail.com>
2026-02-24 18:18:02 -0500

    update1 on uploading pdf

--- uploadPdf.py
+++ uploadPdf.py
@@ -1,9 +1,7 @@
 import pymongo
-import gridfs
-from pathlib import Path
 client=pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["pdf_db"]
-gridObject = gridfs.GridFS(db)
+gridObject = pymongo.gridfs.GridFS(db)
 fileHandle=open("resume.pdf","rb")
 gridObject.put(fileHandle,filename="resume.pdf")
 fileHandle.close()
