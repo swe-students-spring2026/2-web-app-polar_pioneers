6144e7f2bd07f89f8df1a070f4b9caf1d8ec024d
rehanguptaNYU <rehang2005@gmail.com>
2026-02-24 21:29:38 -0500

    new updates

--- uploadPdf.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-client=pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["pdf_db"]
-gridObject = pymongo.gridfs.GridFS(db)
-fileHandle=open("resume.pdf","rb")
-gridObject.put(fileHandle,filename="resume.pdf")
-fileHandle.close()
-
