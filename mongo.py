aa1c11281b4d22ecfd2c64d4a400e9efd2891d04
YASH PAZHIANUR <yrp6541@nyu.edu>
2026-02-26 02:51:35 -0500

    impl proper mongo init

--- mongo.py
+++ mongo.py
@@ -3,30 +3,51 @@ from pymongo.database import Database
 from pymongo.collection import Collection
 from gridfs import GridFSBucket
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["pdf_db"]
+INITIALIZED: bool = False
 
-users = db["users"]
-users.create_index("user_id", unique=True)
-users.create_index("username", unique=True)
-users.create_index("email", unique=True)
+client: MongoClient | None = None
+db: Database | None = None
+resumes: GridFSBucket | None = None
 
-sessions = db["sessions"]
-sessions.create_index("session_id", unique=True)
-sessions.create_index([("user_id", 1), ("input.requested_at", -1)])
-sessions.create_index([("user_id", 1), ("status", 1), ("input.requested_at", -1)])
-sessions.create_index("input.resume_file_id")
+def initMongo(db_uri, db_name):
+    global INITIALIZED, client, db, resumes
 
-resumes = GridFSBucket(db, bucket_name="resumes")
+    if(INITIALIZED): return
+
+    client = MongoClient(db_uri)
+    db = client[db_name]
+
+    users = db["users"]
+    users.create_index("user_id", unique=True)
+    users.create_index("username", unique=True)
+    users.create_index("email", unique=True)
+
+    sessions = db["sessions"]
+    sessions.create_index("session_id", unique=True)
+    sessions.create_index([("user_id", 1), ("input.requested_at", -1)])
+    sessions.create_index([("user_id", 1), ("status", 1), ("input.requested_at", -1)])
+    sessions.create_index("input.resume_file_id")
+
+    resumes = GridFSBucket(db, bucket_name="resumes")
+
+    INITIALIZED = True
+
+def closeMongo():
+    if(not INITIALIZED): raise RuntimeError("mongo not initialized")
+    client.close()
 
 def getDatabase() -> Database:
+    if(not INITIALIZED): raise RuntimeError("mongo not initialized")
     return db
 
 def getCollectionUsers() -> Collection:
-    return users
+    if(not INITIALIZED): raise RuntimeError("mongo not initialized")
+    return db["users"]
 
 def getCollectionSessions() -> Collection:
-    return sessions
+    if(not INITIALIZED): raise RuntimeError("mongo not initialized")
+    return db["sessions"]
 
 def getBucketResumes() -> GridFSBucket:
+    if(not INITIALIZED): raise RuntimeError("mongo not initialized")
     return resumes
